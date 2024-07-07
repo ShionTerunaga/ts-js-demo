8158ba3fb691a3626372fa54cc48c8ec0fb08e03
teru12012000 <sion2769@gmail.com>
2024-07-07 22:05:55 +0900

    fix: delete types

--- src/main/mainSection.data.ts
+++ src/main/mainSection.data.ts
@@ -1,11 +1,4 @@
-import { aboutUndefined } from "../sample/about-undefined"
-import { filterObjectMain } from "../sample/filter-object"
-import { higherOrderFunctionBasic } from "../sample/higher-order-function"
-import { jsIn } from "../sample/js-in"
-import { proxy } from "../sample/proxy"
-import { proxyCall } from "../sample/proxy-cache"
-import { recordSample } from "../sample/record"
-import type { sectionValueType, topicType } from "./main.type"
+import type { sectionValueType } from "./main.type"
 
 export const sectionValue: sectionValueType[] = [
     { name: "proxy基礎編", value: "proxy" },
